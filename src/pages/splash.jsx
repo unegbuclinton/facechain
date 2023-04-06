b115cbe4102369d1cf8e05af051efeb073f51f5a
asjadcommon123 <react.engineer123@gmail.com>
2023-04-06 10:43:16 +0100

    updated favicon

--- src/pages/splash.jsx
+++ src/pages/splash.jsx
@@ -1,37 +1,41 @@
-// import Dependency 
+// import Dependency
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-// import Assets Images 
-import greenLogo from '../assets/images/splash/green-logo.svg'
-import largeStars from '../assets/images/splash/large-stars.svg'
-import moon from '../assets/images/splash/moon.svg'
+// import Assets Images
+import greenLogo from "../assets/images/splash/green-logo.svg";
+import largeStars from "../assets/images/splash/large-stars.svg";
+import moon from "../assets/images/splash/moon.svg";
 
-// import Components 
+// import Components
 import StarBackground from "../components/backgrounds/starbackground";
 
 const SplashScreen = () => {
-   const navigate = useNavigate()
+  const navigate = useNavigate();
 
-   useEffect(()=>{
-      setTimeout(() => {
-         navigate("/login")
-      }, 2000);
-   }, [navigate])
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 2000);
+  }, [navigate]);
 
   return (
-         <StarBackground>
-            <div className="flex flex-col justify-between overflow-hidden">
-               <div className="flex flex-col relative justify-center m-auto">
-                  <img className="self-center absolute" src={greenLogo} alt="green logo" />
-                  <img className="self-center" src={largeStars} alt="large star" />
-               </div>
-               <div>
-                  <img className="sm:w-5/6 sm:m-auto max-w-4xl" src={moon} alt="" />
-               </div>
-            </div>
-         </StarBackground>
-   );
-}
- 
+    <StarBackground>
+      <div className="flex flex-col justify-between overflow-hidden">
+        <div className="flex flex-col relative justify-center m-auto">
+          <img
+            className="self-center absolute"
+            src={greenLogo}
+            alt="green logo"
+          />
+          <img className="self-center" src={largeStars} alt="large star" />
+        </div>
+        <div>
+          <img className="sm:w-5/6 sm:m-auto max-w-4xl" src={moon} alt="" />
+        </div>
+      </div>
+    </StarBackground>
+  );
+};
+
 export default SplashScreen;
