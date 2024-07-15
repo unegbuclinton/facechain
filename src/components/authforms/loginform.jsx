43d092de2922d4e83ec6521962a257720a02cd05
unegbu Clinton <unegbuclinton5@gmail.com>
2024-07-15 10:17:29 +0100

    update master

--- src/components/authforms/loginform.jsx
+++ src/components/authforms/loginform.jsx
@@ -1,131 +1,129 @@
 // Import Dependency
-import {
-  faEye,
-  faEyeSlash,
-  faSpinner,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { faEye, faEyeSlash, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 // Import Assts Images
-import googleIcon from "../../assets/images/login/google-icon.svg";
-import { loggedInUser } from "../../redux/authSlice";
+import googleIcon from '../../assets/images/login/google-icon.svg'
+import { loggedInUser } from '../../redux/authSlice'
 
 const LoginForm = () => {
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const [inputValues, setInputValues] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const dispatch = useDispatch();
-  const { authPass } = useSelector((state) => state.auth);
+  const navigate = useNavigate()
+  const [show, setShow] = useState(false)
+  const [inputValues, setInputValues] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const dispatch = useDispatch()
+  const { authPass } = useSelector((state) => state.auth)
   function toggleVisiblity() {
-    setShow((prevShow) => !prevShow);
+    setShow((prevShow) => !prevShow)
   }
 
   const handleChange = (e) => {
-    setInputValues({ ...inputValues, [e.target.name]: e.target.value });
-  };
+    setInputValues({ ...inputValues, [e.target.name]: e.target.value })
+  }
 
   function submitForm(e) {
-    e.preventDefault();
-    setIsLoading(true);
-    if (!inputValues) return toast.error("Please fill in all fields");
-    const { email, password } = inputValues;
+    e.preventDefault()
+    setIsLoading(true)
+    if (!inputValues) return toast.error('Please fill in all fields')
+    const { email, password } = inputValues
     const authUser = authPass.find(
       (user) => user.email === email && user.password === password
-    );
+    )
     if (authUser) {
-      const data = { email, password };
-      dispatch(loggedInUser(data));
-      setIsLoading(false);
-      navigate("/home");
+      const data = { email, password }
+      dispatch(loggedInUser(data))
+      setIsLoading(false)
+      navigate('/home')
     } else {
-      toast.error("Username/Password is not correct");
-      setIsLoading(false);
+      toast.error(
+        'Please use this detail auth pass : Username:test@test.com, password:Password12345%'
+      )
+      setIsLoading(false)
     }
   }
 
   return (
-    <div className="text-white text-center sm:px-40 flex flex-col justify-evenly flex-1">
-      <p className="font-buttons text-primary font-bold text-xl">Moon Rider</p>
-      <h2 className="text-white font-buttons text-3xl font-extrabold">
+    <div className='text-white text-center sm:px-40 flex flex-col justify-evenly flex-1'>
+      <p className='font-buttons text-primary font-bold text-xl'>Moon Rider</p>
+      <h2 className='text-white font-buttons text-3xl font-extrabold'>
         Log in
       </h2>
-      <form action="" className="auth-form">
-        <div className="relative">
+      <form action='' className='auth-form'>
+        <div className='relative'>
           <input
-            className="auth-input placeholder-transparent peer"
-            name="email"
-            type="text"
-            placeholder="example@mail.com"
+            className='auth-input placeholder-transparent peer'
+            name='email'
+            type='text'
+            placeholder='example@mail.com'
             required
             onChange={handleChange}
           />
-          <label className="float-label transition-all">
+          <label className='float-label transition-all'>
             Email/Phone number
           </label>
         </div>
-        <div className="relative">
+        <div className='relative'>
           <input
-            className="auth-input placeholder-transparent peer"
-            name="password"
-            type={show ? "text" : "password"}
-            placeholder="example@mail.com"
+            className='auth-input placeholder-transparent peer'
+            name='password'
+            type={show ? 'text' : 'password'}
+            placeholder='example@mail.com'
             required
             onChange={handleChange}
           />
-          <label className="float-label transition-all">Password</label>
+          <label className='float-label transition-all'>Password</label>
           <FontAwesomeIcon
             icon={show ? faEyeSlash : faEye}
-            className="absolute cursor-pointer right-5 top-4"
+            className='absolute cursor-pointer right-5 top-4'
             onClick={toggleVisiblity}
           />
         </div>
-        <Link to="/forgot-password">
-          <p className="text-primary text-right mb-5 font-bold text-xs cursor-pointer">
+        <Link to='/forgot-password'>
+          <p className='text-primary text-right mb-5 font-bold text-xs cursor-pointer'>
             Forgot Password?
           </p>
         </Link>
         <button
           onClick={submitForm}
-          className="bg-primary font-buttons text-black text-lg font-bold w-full rounded-full py-3"
+          className='bg-primary font-buttons text-black text-lg font-bold w-full rounded-full py-3'
         >
           {isLoading ? (
             <FontAwesomeIcon
               icon={faSpinner}
-              className=" cursor-pointer fa-spin fa-pulse"
+              className=' cursor-pointer fa-spin fa-pulse'
             />
           ) : (
-            "Log In"
+            'Log In'
           )}
         </button>
       </form>
-      <div className="flex flex-row justify-between items-center text-greyTextSecondary">
-        <hr className="w-1/3 border-t border-gray-500" />
-        <p className="text-xs">Or Sign in With</p>
-        <hr className="w-1/3 border-t border-gray-500" />
+      <div className='flex flex-row justify-between items-center text-greyTextSecondary'>
+        <hr className='w-1/3 border-t border-gray-500' />
+        <p className='text-xs'>Or Sign in With</p>
+        <hr className='w-1/3 border-t border-gray-500' />
       </div>
       <div>
-        <button className="text-sm text-white sm:text-lg w-full border px-10 rounded-full flex flex-row justify-center py-3">
-          <img className="self-center mr-5" src={googleIcon} alt="" />
+        <button className='text-sm text-white sm:text-lg w-full border px-10 rounded-full flex flex-row justify-center py-3'>
+          <img className='self-center mr-5' src={googleIcon} alt='' />
           Sign in with Google
         </button>
       </div>
 
       <div>
-        <p className="text-xs text-greyTextSecondary">
-          {" "}
-          Don't have an account?{" "}
-          <Link to="/signup">
-            <span className="text-primary cursor-pointer">Sign up</span>
+        <p className='text-xs text-greyTextSecondary'>
+          {' '}
+          Don't have an account?{' '}
+          <Link to='/signup'>
+            <span className='text-primary cursor-pointer'>Sign up</span>
           </Link>
         </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LoginForm;
+export default LoginForm
--- src/redux/authSlice.jsx
+++ src/redux/authSlice.jsx
@@ -1,46 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { toast } from "react-toastify/dist";
+import { createSlice } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify/dist'
 
 const initialState = {
   authPass: [
-    { fullname: "Tests", email: "test@test.com", password: "Password12345%" },
+    { fullname: 'Tests', email: 'test@test.com', password: 'Password12345%' },
   ],
   isLoading: false,
   user: {},
-};
+}
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     createNewUser: (state, action) => {
-      const { fullname, email, password } = action.payload;
+      const { fullname, email, password } = action.payload
       state.authPass.map((item) => {
         if (item.email === email) {
-          return toast.error("User already exist");
+          return toast.error('User already exist')
         } else {
-          return state.authPass.push({ fullname, email, password });
+          return state.authPass.push({ fullname, email, password })
         }
-      });
+      })
     },
     loggedInUser: (state, action) => {
-      const { email } = action.payload;
-      state.authPass.find((user, i) => {
-        if (user.email === email) {
-          state.user = {
-            userName: state.authPass[i].fullname,
-            userEmail: state.authPass[i].email,
-          };
-
-          return true; // stop searching
+      const { email, password } = action.payload
+      const user =
+        state.authPass[0].user === email &&
+        state.authPass[0].password === password
+      if (user) {
+        state.user = {
+          userName: state.authPass[0].fullname,
+          userEmail: state.authPass[0].email,
         }
-        return null;
-      });
+      }
+      return
+      // state.authPass.find((user, i) => {
+      //   if (user.email === email) {
+      //     state.user = {
+      //       userName: state.authPass[i].fullname,
+      //       userEmail: state.authPass[i].email,
+      //     };
+
+      //     return true; // stop searching
+      //   }
+      //   return null;
+      // });
     },
   },
 
   extraReducers: (builder) => {},
-});
+})
 
-export const { createNewUser, loggedInUser } = authSlice.actions;
-export default authSlice.reducer;
+export const { createNewUser, loggedInUser } = authSlice.actions
+export default authSlice.reducer
